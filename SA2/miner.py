758453bee134b69fe008bc3d768dac3e0de0d25f
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-08-24 10:41:31 +0530

    Add files via upload

---
+++ SA2/miner.py
@@ -0,0 +1,25 @@
+from time import time
+from blockchain import Block
+
+class Miner():
+    def __init__(self, address):
+        self.address= address
+        self.walletBalance = 0    
+   
+    # Define createBlock method that takes index of the block and transactions list
+    
+        # Check if transactions length greater then 3
+        
+            # Get first three transactions in transactions variable
+            
+
+            # Take this code from commented code in app.py same done in previous classes
+            
+
+            # Add transactions to currentBlock.transactions     
+
+            # Return the current block
+            
+
+        # Return false if transactions are less then 3
+        
